6d2afdd42ed8289b4e1833121bc754bdb4045993
blubtxt <admin@devwerks.net>
2014-01-20 09:37:03 +0100

    added new query
    Added Query which requests the VirusTotal results for the Top4 files. (
    Public API limited to 4 requests per minute )

--- glastopf-stats.py
+++ glastopf-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sqlite3, sys, getopt
+import sqlite3, sys, getopt, simplejson, urllib, urllib2
 
 #
 #	Basic Python script for print out some Stats of your Glastopf Honeypot
@@ -10,15 +10,30 @@ import sqlite3, sys, getopt
 #Change here the path to your Glastopf Database
 dbfileglastopf = '/opt/myhoneypot/db/glastopf.db'
 
-def executeQuery(query):
-        
+#Change here your VirusTotal API Key *Public API limited to 4 requests per minute*
+vtapikey = '36f43b91fc85ea8d5c0dcbf7b3089d9a4538069c25dc7f93f1d64354060b9f0a'
+
+#VirusTotal URL 
+vturl = "https://www.virustotal.com/vtapi/v2/file/report"
+            
+def executeQuery(query,num):
+    
         conn = sqlite3.connect(dbfileglastopf)
         c = conn.cursor()
         
-	c.execute(query)
+        c.execute(query)
 	
 	for row in c:
-            print(row)
+            if num == '8':
+                parameters = {"resource": row[1], "apikey": vtapikey}
+                data = urllib.urlencode(parameters)
+                req = urllib2.Request(vturl, data)
+                response = urllib2.urlopen(req)
+                json = response.read()
+                response_dict = simplejson.loads(json)
+                print row,response_dict.get("positives"),response_dict.get("total")
+            else: 
+                print(row)
     
 def selectQuery():
     
@@ -44,49 +59,56 @@ def selectQuery():
                     sys.stdout.write("#Attacks over last 30 Days\n")
                     querySQL = 'SELECT COUNT(time), SUBSTR(time,-20,12) AS stripped FROM events GROUP BY stripped ORDER BY stripped DESC LIMIT 30'
                     sys.stdout.write("\nQuery: %s\nHits | Date\n" %(querySQL))
-                    executeQuery(querySQL)
+                    executeQuery(querySQL,arg)
                     
                 #Last 10 events
                 elif arg == '2':
                     sys.stdout.write("#Last 10 events\n")
                     querySQL = 'SELECT time,request_url FROM events ORDER BY time DESC LIMIT 10'
                     sys.stdout.write("\nQuery: %s\nTime | Url\n" %(querySQL))
-                    executeQuery(querySQL)
+                    executeQuery(querySQL,arg)
                 
                 #Top10 files
                 elif arg == '3':
                     sys.stdout.write("#Top10 files\n")
                     querySQL = 'SELECT COUNT(filename), filename FROM events GROUP BY filename ORDER BY COUNT(filename) DESC LIMIT 10'
-                    sys.stdout.write("\nQuery: %s\nNum | Hash\n" %(querySQL))
-                    executeQuery(querySQL)
+                    sys.stdout.write("\nQuery: %s\nNum | Hash | Virustotal\n" %(querySQL))
+                    executeQuery(querySQL,arg)
                 
                 #Busy Attackers
                 elif arg == '4':
                     sys.stdout.write("#Busy Attackers\n")
                     querySQL = 'SELECT COUNT(source), SUBSTR(source,-20,14) AS stripped FROM events GROUP BY stripped ORDER BY COUNT(stripped) DESC LIMIT 10'
                     sys.stdout.write("\nQuery: %s\nHits | Host\n" %(querySQL))
-                    executeQuery(querySQL)
+                    executeQuery(querySQL,arg)
                 
                 #Top15 intext requests
                 elif arg == '5':
                     sys.stdout.write("#Top15 intext requests\n")
                     querySQL = 'SELECT count, content FROM intext ORDER BY count DESC LIMIT 15'
                     sys.stdout.write("\nQuery: %s\nHits | Request\n" %(querySQL))
-                    executeQuery(querySQL)
+                    executeQuery(querySQL,arg)
                 
                 #Top15 intitle requests
                 elif arg == '6':
                     sys.stdout.write("#Top15 intitle requests\n")
                     querySQL = 'SELECT count, content FROM intitle ORDER BY count DESC LIMIT 15'
                     sys.stdout.write("\nQuery: %s\nHits | Request\n" %(querySQL))
-                    executeQuery(querySQL)
+                    executeQuery(querySQL,arg)
                 
                 #Top10 inurl requests
                 elif arg == '7':
                     sys.stdout.write("#Top10 inurl requests\n")
                     querySQL = 'SELECT count, content FROM inurl ORDER BY count DESC LIMIT 10'
                     sys.stdout.write("\nQuery: %s\nHits | Request\n" %(querySQL))
-                    executeQuery(querySQL)
+                    executeQuery(querySQL,arg)
+                    
+                #Top4 files with Virustotal results *Public API limited to 4 requests per minute*
+                elif arg == '8':
+                    sys.stdout.write("#Top4 files with VirusTotal results\n")
+                    querySQL = 'SELECT COUNT(filename), filename FROM events GROUP BY filename ORDER BY COUNT(filename) DESC LIMIT 4'
+                    sys.stdout.write("\nQuery: %s\nNum | Hash | Virustotal\n" %(querySQL))
+                    executeQuery(querySQL,arg)
 
 def version():
     
@@ -104,6 +126,7 @@ def help():
     sys.stdout.write("5:Top15 intext requests\n")
     sys.stdout.write("6:Top15 intitle requests\n")
     sys.stdout.write("7:Top10 inurl requests\n\n")
+    sys.stdout.write("8:Top4 files with VT results\n\n")
 
 def main():
     
